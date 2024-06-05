732816ccba91298002aecc97fbe15927a809e73e
nnnnnnniam1 <nnnnnnniam1@example.com>
2024-06-05 09:36:20 +0900

    fix: pjtDetail 타입 수정

--- src/config/db/tb.pjt.js
+++ src/config/db/tb.pjt.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING(255),
       },
       PJT_DETAIL: {
-        type: DataTypes.BLOB,
+        type: DataTypes.TEXT,
       },
       PJT_STTS: {
         type: DataTypes.STRING(45),
