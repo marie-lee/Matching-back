2658eb8e33cee62f6a4cac8873fe10787f977d9e
nnnnnnniam1 <nnnnnnniam1@example.com>
2024-06-10 12:03:19 +0900

    fix: matching 데이터 null 예외처리

--- src/utils/matching/matching.py
+++ src/utils/matching/matching.py
@@ -35,38 +35,45 @@ def get_average_embedding(text_list):
 
 # 프로젝트 데이터 가공 함수
 def process_description(description):
-    sentences = description.split('\n')
+    sentences = description.split('/n')
     processed_sentences = []
+    # 문장이 하나만 있는 경우
+    if len(sentences) == 1:
+        return description
     for sentence in sentences:
         if len(sentence) > 50:
-            processed_sentences.append(sentence[:50])
+            for i in range(0, len(sentence), 50):
+                processed_sentences.append(sentence[i:i+50])
         else:
             processed_sentences.append(sentence)
     return processed_sentences
 
 # 프로필 데이터를 텍스트로 변환
 def profile_to_text(profile):
-    text = f"{process_description(profile['introduction'])}, {profile['stack']}, {profile['interests']}"
+    text = []
+    text.append(process_description(profile['introduction']) if profile.get('introduction') else '') # 프로필 소개
+    text.append(process_description(profile['stack']) if profile.get('stack') else '') # 사용 기술 스택
+    text.append(process_description(profile['interests']) if profile.get('interests') else '') # 관심 기술
     if profile.get("career"):
         for exp in profile["career"]:
-            text += f", {exp['careerNm']}, {exp['enteringDt']}, {exp.get('quitDt', '')}"
+            text.append(f"{exp.get('careerNm', '')}, {exp.get('enteringDt', '')}, {exp.get('quitDt', '')}") # 경력
     return text
 
 # 프로필 벡터 계산
-def profile_to_vector(pf_sn, profile):
+def profile_to_vector(pfSn, profile):
     profile_text = profile_to_text(profile)
-    average_embedding = get_average_embedding([profile_text])
-    return average_embedding.squeeze(), pf_sn  # 프로필 일련번호과 반환
+    average_embedding = get_average_embedding(profile_text)
+    return average_embedding.squeeze(), pfSn  # 프로필 일련번호과 반환
 
 # 포트폴리오 데이터 가공 함수
 def portfolio_to_text(portfolio):
     processed_texts = []
     for project in portfolio:
         project_texts = []
-        project_texts.append(process_description(project['introduction'])) # 프로젝트 소개의 처음 50자
-        project_texts.append(process_description(project['stack'])) # 스택 정보의 처음 50자
-        project_texts.append(process_description(project['role'])) # 역할 정보의 처음 50자
-        project_texts.append(f"contribution: {project['contribution']}") # 기여도 정보의 처음 50자만 가져옴
+        project_texts.append("".join(process_description(project['introduction'])) if project.get('introduction') else '') # 프로젝트 소개
+        project_texts.append("".join(process_description(project['stack'])) if project.get('stack') else '') # 스택 정보
+        project_texts.append("".join(process_description(project['role'])) if project.get('role') else '')  # 역할 정보
+        project_texts.append(f"contribution: {project.get('contribution', '')}" if project.get('contribution') else '')    # 기여도 정보
         processed_texts.append(project_texts)
     return processed_texts
 
@@ -74,8 +81,7 @@ def portfolio_to_text(portfolio):
 def portfolio_to_vector(portfolio):
     portfolio_text = portfolio_to_text(portfolio)
     project_vectors =  [get_average_embedding(project).squeeze()  for project in portfolio_text]
-    portfolio_vector = np.mean(project_vectors, axis=0)
-    return portfolio_vector
+    return np.mean(project_vectors, axis=0)
 
 # 코사인 유사도 계산 함수
 def cosine_similarity(a, b):
@@ -87,7 +93,7 @@ def main():
         #  프로젝트 데이터 매개변수에서 가져오기
         project_data = json.loads(sys.argv[1])
         #  회원 프로필 포트폴리오 데이터
-        # url = "http://localhost:8080/api/recommendation/memberData"
+        # url = "http://localhost:8000/api/recommendation/memberData"
         url = "http://218.232.137.30:20080/api/recommendation/memberData"
         response = requests.get(url)
 
@@ -104,30 +110,29 @@ def main():
 
         #  프로젝트 데이터 가공
         project_texts = []
-
-        project_texts.append(project_data['pjtIntro']) # 프로젝트 간단 정보
-        project_texts.extend(process_description(project_data['pjtDetail']))
-        parts = [role['part'] for role in project_data['role']]
-        project_texts.append(', '.join(parts))  # 리스트를 문자열로 변환
-        project_texts.append(project_data['stack'])
-        project_texts.append(', '.join(project_data['experience']))
-        project_name = project_data['pjtNm']
+        project_texts.append(process_description(project_data['pjtIntro']) if project_data.get('pjtIntro') else '') # 프로젝트 간단 정보
+        project_texts.append(process_description(project_data['pjtDetail']) if project_data.get('pjtDetail') else '') # 프로젝트 상세 정보
+        project_texts.append(process_description(project_data['pjtIntro']) if project_data.get('pjtIntro') else '') # 프로젝트 간단 정보
+        project_texts.append(', '.join([role['part'] for role in project_data['role']]) if project_data.get('role') else '') # 모집 파트
+        project_texts.append(process_description(project_data['stack']) if project_data.get('stack') else '') # 프로젝트 스택
+        project_texts.append(', '.join(project_data.get('experience', '')) if project_data.get('experience') else '') # 원하는 경험상
 
         # 프로젝트 벡터 계산
         project_vector = get_average_embedding(project_texts).squeeze()
 
         similar_profiles = []
         for member in profile_data:
-            pf_sn = member['pfSn']
+            pfSn = member['pfSn']
             profile = member['profile']
-            profile_vector, profile_sn = profile_to_vector(pf_sn, profile)  # 프로필 벡터화
+            profile_vector, profile_sn = profile_to_vector(pfSn, profile)  # 프로필 벡터화
             portfolio_vector = portfolio_to_vector(member['portfolio'])        # 포트폴리오 벡터화
 
             member_vector = np.mean([profile_vector, portfolio_vector], axis=0) # 프로필과 포트폴리오 벡터 연결
+
             similarity = cosine_similarity(member_vector, project_vector)
 
             if similarity >= 0.6:
-                similar_profiles.append((pf_sn, similarity))
+                similar_profiles.append((pfSn, similarity))
 
         # 유사도가 높은 순서대로 정렬
         similar_profiles.sort(key=lambda x: x[1], reverse=True)
