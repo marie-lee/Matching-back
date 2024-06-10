5efd33580a030d7aefd097bf6e8901c4eac7c0b8
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-06-10 13:21:31 +0900

    Revert "Prototype edit yn (#17)"
    This reverts commit cd11e5dee9771f88c30379ec5ec0ea8fed4d4b00.

--- src/config/swagger/index.js
+++ src/config/swagger/index.js
@@ -1,5 +1,37 @@
 const swaggerUi = require("swagger-ui-express");
-const yamljs = require('yamljs');
-const swaggerDocument = yamljs.load(__dirname+'/swagger.yaml');
+const swaggerJsdoc = require("swagger-jsdoc");
 
-module.exports = { swaggerUi, swaggerDocument };
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0", // OpenAPI 버전 명시
+    components: {
+      securitySchemes: {
+        Authorization: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    info: { 
+      title: "MATCHING API",
+      version: process.env.VERSION,
+      description: "MATCHING Backend API",
+    },
+    security: [
+      {
+        Authorization: [],
+      },
+    ],
+    servers: [
+      {
+        url: "http://localhost:8080/",
+      },
+    ],
+  },
+    apis: ["./src/app.js", "./src/route/*.js"], // api는 /routes 파일 아래 js 파일 내에 정의하고 있으며, /swagger 폴더 아래 swagger 설정을 정의하고 있음
+  };
+  
+const specs = swaggerJsdoc(options);
+  
+module.exports = { swaggerUi, specs };
