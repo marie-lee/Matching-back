f9731bb7069e22637cf3b673b325c674dc568d3c
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-06-12 01:56:32 +0900

    feat: 현황-프로젝트 조회

--- src/api/status/status.ctrl.js
+++ src/api/status/status.ctrl.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const statusService = require('./status.service');
 const jwt = require('../../utils/jwt/jwt');
 const {logger} = require('../../utils/logger');
+const projectService = require("../projects/project.service");
 
 router.get('/status', jwt.authenticateToken, async (req, res) => {
    try{
@@ -13,5 +14,17 @@ router.get('/status', jwt.authenticateToken, async (req, res) => {
        return res.status(400).send('현황 조회 중 에러 : '+error);
    }
 });
+
+router.get('/status/:pjtSn',jwt.authenticateToken, async (req, res) => {
+    try{
+        const userSn = req.userSn.USER_SN;
+        const pjtSn = req.params.pjtSn;
+        const pjtData = await projectService.reqProject(userSn, pjtSn);
+        return res.status(200).send(pjtData[0]);
+    }catch (error) {
+        logger.error(`내 프로젝트 조회 실패: ${error}`);
+        return res.status(400).send(`내 프로젝트 조회 실패 :  ${error}`);
+    }
+})
 
 module.exports = router;
