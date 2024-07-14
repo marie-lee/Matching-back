e19f22e0aa96f74d856dbb5f6ee3b63a205a82f0
Jimin-2 <105487729+Jimin-2@users.noreply.github.com>
2024-07-15 03:22:34 +0900

    feat: 비밀번호 찾기, 비밀번호 변경

--- src/api/member/member.ctrl.js
+++ src/api/member/member.ctrl.js
@@ -22,6 +22,7 @@ router.post('/registeration/join',async (req,res)=>{
 });
 
 router.post('/registeration/certification',async (req,res)=>{
+  req.body.PURPOSE = 'register';
   try{
     return await MemberService.requestEmail(req,res);
   } catch (error) {
@@ -31,6 +32,7 @@ router.post('/registeration/certification',async (req,res)=>{
 });
 
 router.post('/registeration/confirmation',async (req,res)=>{
+  req.body.PURPOSE = 'register';
   try{
     return await MemberService.verifyEmailCode(req,res);
   } catch (error) {
@@ -51,6 +53,35 @@ router.post('/find/id',async (req,res)=>{
   } catch (error){
     logger.error('아이디 찾기 실패:',error);
     return res.status(400).send('아이디 찾기 실패:'+error.message);
+  }
+});
+
+router.post('/find/pw', async (req, res) => {
+  req.body.PURPOSE = 'reset_password';
+  try {
+    return await MemberService.findPassword(req, res);
+  } catch (error) {
+    logger.error('비밀번호 찾기 요청 실패:', error);
+    return res.status(400).send('비밀번호 찾기 요청 실패: ' + error.message);
+  }
+});
+
+router.post('/find/pw/certification', async (req, res) => {
+  req.body.PURPOSE = 'reset_password';
+  try {
+    return await MemberService.verifyEmailCode(req, res);
+  } catch (error) {
+    logger.error('비밀번호 찾기 인증 실패:', error);
+    return res.status(400).send('비밀번호 찾기 인증 실패: ' + error.message);
+  }
+});
+
+router.post('/find/pw/confirmation', async (req, res) => {
+  try {
+    return await MemberService.confirmPasswordReset(req, res);
+  } catch (error) {
+    logger.error('비밀번호 재설정 실패:', error );
+    return res.status(400).send('비밀번호 재설정 실패: ' + error.message);
   }
 });
 
