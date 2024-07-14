0fabf3a768ed1737f0cdae1503ebd556d6c106c4
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-07-14 22:02:03 +0900

    feat : 프로젝트 wbs 조회

--- src/api/common/common.service.js
+++ src/api/common/common.service.js
@@ -4,20 +4,21 @@ const {QueryTypes} = require("sequelize");
 const {throwError} = require("../../utils/errors");
 const {getProjectIntro} = require("../projects/project.service");
 const {error} = require("winston");
+const moment = require("moment")
 
-class commonService {
-
-  async oneCmmnVal(cmmnCdType, cmmnCd){
-    console.log(cmmnCd)
-    try{
-      return await db.TB_CMMN_CD.findOne({
-        where: {CMMN_CD_TYPE: cmmnCdType, CMMN_CD: cmmnCd},
-        attributes: ['CMMN_CD_VAL'],
-      })
-    } catch (error){
-      throw error;
-    }
+const oneCmmnVal = async (cmmnCdType, cmmnCd) => {
+  try {
+    return await db.TB_CMMN_CD.findOne({
+      where: {CMMN_CD_TYPE: cmmnCdType, CMMN_CD: cmmnCd},
+      attributes: ['CMMN_CD_VAL'],
+    })
+  } catch (error) {
+    throw error;
   }
 }
 
-module.exports = new commonService();
+const formatDt = (dt) => {
+  return moment(dt).format('YYYY-MM-DD HH:mm:ss');
+};
+
+module.exports = {oneCmmnVal, formatDt};
--- src/utils/errors.js
+++ src/utils/errors.js
@@ -2,7 +2,7 @@ const {logger} = require('./logger');
 
 const throwError = (message) => {
     logger.error(message);
-    throw new   Error(message);
+    throw new Error(message);
 }
 
 module.exports = {throwError}
