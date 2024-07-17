d2fdb14a7820a5235e00c48c36b4a3c6483b1985
Jimin-2 <105487729+Jimin-2@users.noreply.github.com>
2024-07-17 16:52:03 +0900

    fix: 내 프로젝트 목록 조회

--- src/api/projects/project.ctrl.js
+++ src/api/projects/project.ctrl.js
@@ -17,7 +17,9 @@ router.get('/project/all', async (req, res) => {
 
 router.get('/project', jwt.authenticateToken, async (req, res) => {
     try {
-        return await projectService.myProjects(req, res);
+      const userSn = req.userSn.USER_SN;
+      const myProjects = await projectService.myProjects(userSn);
+      return res.status(200).send(myProjects);
     } catch (error) {
         logger.error('내 프로젝트 리스트 조회 실패', error);
         return res.status(400).send('내 프로젝트 리스트 조회 실패 : ' + error);
