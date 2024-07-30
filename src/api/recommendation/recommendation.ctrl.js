daaf2493c05a5c32cb4428ba95b405d4e06411f8
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2024-07-31 00:43:44 +0900

    fix : 매칭 코드 수정

--- src/api/recommendation/recommendation.ctrl.js
+++ src/api/recommendation/recommendation.ctrl.js
@@ -3,26 +3,17 @@ const router = express.Router();
 const recommendationService = require('./recommendation.service');
 const jwt = require('../../utils/jwt/jwt');
 const {logger} = require('../../utils/logger');
-
 router.get('/recommendation/project/:pjtSn', jwt.authenticateToken, async (req, res)=>{
+    const pjt = req.params.pjtSn;
+    const user = req.userSn.USER_SN;
     try{
-        return await recommendationService.selectMatchingData(req, res);
+        const matchingResult =  await recommendationService.selectMatchingData(user, pjt);
+        res.status(200).json(matchingResult);
     }catch (error) {
         logger.error('매칭 - 추천 회원 프로필/포트폴리오 조회 실패', error);
-        return res.status(400).send('매칭 - 추천 회원 프로필/포트폴리오 조회 실패 : ' + error);
+        return res.status(400).json({message : '매칭 - 추천 회원 프로필/포트폴리오 조회 실패 : ' + error});
     }
 });
-
-router.get('/recommendation/memberData', async (req, res)=>{
-    try{
-        return await recommendationService.selectMemberData(req, res);
-    }catch (error) {
-        logger.error('회원 프로필 포트폴리오 조회 실패', error);
-        return res.status(400).send('회원 프로필 포트폴리오 조회 실패 : ' + error);
-    }
-})
-
-
 
 
 module.exports = router;
