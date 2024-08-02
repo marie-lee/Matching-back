089d8d620deb439b739ddfe30d90bc7011487b79
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2024-08-02 19:13:04 +0900

    feat : 이슈추가

--- src/api/wbs/dto/issue.create.dto.js
+++ src/api/wbs/dto/issue.create.dto.js
@@ -1,13 +1,13 @@
 class CreateIssueDto {
     constructor({
-                    pjtSn, userSn,ticketSn, title, periority, contents,mentions, status
+                    pjtSn, userSn,ticketSn, issueNm, priority, content, mentions, status
     }) {
         this.PJT_SN = pjtSn;
         this.USER_SN = userSn;
         this.TICKET_SN = ticketSn;
-        this.TITLE = title;
-        this.PERIORITY = periority;
-        this.CONTENTS = contents;
+        this.ISSUE_NM = issueNm;
+        this.PRIORITY = priority;
+        this.CONTENT = content;
         this.MENTIONS = mentions;
         this.STATUS = status;
 
@@ -18,8 +18,8 @@ class CreateIssueDto {
             PJT_SN: '프로젝트가 없습니다.',
             USER_SN: '회원 정보가 없습니다.',
             TICKET_SN: '티켓 정보가 없습니다.',
-            TITLE: '이슈 타이틀이 입력되지 않았습니다.',
-            CONTENTS: '이슈 내용이입력되지 않았습니다.',
+            ISSUE_NM: '이슈 타이틀이 입력되지 않았습니다.',
+            CONTENT: '이슈 내용이입력되지 않았습니다.',
         };
         for (const [field, message] of Object.entries(requiredFields)) {
             if (this[field] === '' || this[field] === null || this[field] === undefined) {
