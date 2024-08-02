b175c6d7914871137d24184261fa712127634249
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2024-08-03 01:02:38 +0900

    feat : 이슈 상태 및 중요도 수정 dto 추가

---
+++ src/api/wbs/dto/issue.dto.js
@@ -0,0 +1,23 @@
+class IssueDto {
+    constructor({
+                pjtSn, userSn, issueSn, priority = null, status = null}) {
+        this.PJT_SN = pjtSn;
+        this.USER_SN = userSn;
+        this.ISSUE_SN = issueSn;
+        this.updateIssueData = new UpdateIssueDto({priority, status})
+    }
+    validation(){
+        if(this.updateIssueData.PRIORITY == null && this.updateIssueData.STATUS == null){
+            throw Error('업데이트할 내용이 없습니다.');
+        }
+    }
+}
+
+class UpdateIssueDto {
+    constructor({priority = null, status = null}) {
+        this.PRIORITY = priority;
+        this.STATUS = status;
+    }
+
+}
+module.exports = IssueDto;
